ea2a93e9564a78bcf934784a1550f7da9da2cf7d
y-kurami <yosuke.kurami@gmail.com>
2017-08-19 02:21:53 +0900

    Capture console, exit code, refactor

--- puppeteer-test.js
+++ puppeteer-test.js
@@ -9,7 +9,7 @@ const mkdirp = require('mkdirp');
 const rimraf = require('rimraf');
 
 class Emitter extends EventEmitter {
-  refreshFiles() {    // Called by karma plugin
+  refreshFiles() {          // Called by karma plugin
     this.emit('ready');
   }
 }
@@ -17,28 +17,44 @@ class Emitter extends EventEmitter {
 function createServer(port) {
   const app = express();
   const emitter = new Emitter();
-  const handlers = []; // karm plugin will push something like { handler: webpack-dev-middleware }.
+  const handlers = [];      // karm plugin will push something like { handler: webpack-dev-middleware }.
   const testConfig = karma.config.parseConfig(path.resolve('./karma.conf.js'));
   const init = ngCLIKarmaPlugin['framework:@angular/cli'][1];
   init(testConfig, emitter, handlers); // conf, emitter, customFileHandlers,
   app.use(express.static(__dirname));
   app.use(handlers[0].handler);
-  return new Promise((resolve, reject) => emitter.once('ready', () => app.listen(port, () => resolve(emitter))));
+  const server = {
+    app,
+    shutdown: () => new Promise(resolve => emitter.emit('exit', resolve)),
+  };
+  return new Promise((resolve, reject) => emitter.once('ready', () => app.listen(port, () => resolve(server))));
 }
 
-(async() => {
-  rimraf.sync('actual_images/**/*.png');
-  mkdirp.sync('actual_images');
+async function runPuppeteer() {
   const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   const page = await browser.newPage();
+
+  // Capture logging
+  page.on('console', (...args) => console.log.apply(console, ['[Browser]', ...args]));
+
+  // Expose Screenshot function
   await page.exposeFunction('capturePage', name => {
     const filename = path.resolve(__dirname, `actual_images/${name}.png`);
-    console.log('Save 🎨 to', filename);
+    console.log('[Node]', 'Save 🎨  to', filename);
     return page.screenshot({ path: filename, fullPage: true });
   });
-  const emitter = await createServer(3100);
+
+  const server = await createServer(3100);
+
   await page.exposeFunction('puppeteerDone', async code => {
-    emitter.emit('exit', async () => process.exit(code || 0));
+    await server.shutdown();
+    process.exit(code);
   });
+
   await page.goto('http://localhost:3100/test/context.html');
-})();
+}
+
+rimraf.sync('actual_images/**/*.png');
+mkdirp.sync('actual_images');
+
+runPuppeteer();
--- src/test.ts
+++ src/test.ts
@@ -13,6 +13,9 @@ import {
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
 
+const { SpecReporter } = require('jasmine-spec-reporter');
+jasmine.getEnv().addReporter(new SpecReporter());
+
 // Unfortunately there's no typing for the `__karma__` variable. Just declare it as any.
 declare const __karma__: any;
 declare const require: any;
